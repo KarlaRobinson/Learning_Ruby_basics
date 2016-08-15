849161f431c89b96df09e0a761b442626400d590
codeamac03 <codeamac03@gmail.com>
2016-08-15 16:13:23 -0500

    create def dictionary_sort

---
+++ phase_one/DiccionarioOrdenado.rb
@@ -0,0 +1,44 @@
+def dictionary_sort
+  store_words = []
+  puts "Escribe una palabra:"
+  user_word = gets.chomp
+  while user_word != ""
+    store_words << user_word
+    puts "Escribe otra palabra(o presiona 'enter' para finalizar):"
+    user_word = gets.chomp
+  end
+  puts "Felicidades! Tu diccionario tiene #{store_words.length} palabras: "
+  puts store_words.sort_by {|word| word.downcase}
+end
+
+ dictionary_sort
+
+# Escribe una palabra:   
+# > Gato
+# > Escribe otra palabra(o presiona "enter" para finalizar):   
+# > Lechuga
+# > Escribe otra palabra(o presiona "enter" para finalizar):   
+# > Rojo
+# > Escribe otra palabra(o presiona "enter" para finalizar):   
+# > Silla
+# > Escribe otra palabra(o presiona "enter" para finalizar):   
+# > Armadillo
+# >
+# > Felicidades! Tu diccionario tiene 5 palabras: 
+# > Armadillo
+# > Gato
+# > Lechuga
+# > Rojo
+# > Silla
+
+
+
+
+
+
+
+
+
+
+
+
