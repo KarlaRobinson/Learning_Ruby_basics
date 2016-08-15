2f8bc7b40301720346210c036fa7994d5c0a8196
codeamac03 <codeamac03@gmail.com>
2016-08-15 14:25:55 -0500

    Refactored

---
+++ phase_one/chaining_methods.rb
@@ -0,0 +1,16 @@
+# TODO: Refactoriza buscando elegancia 
+def shout_backwards(string)
+  string.upcase.reverse + "!!!"
+end
+
+# AYUDAME: Refactoriza buscando elegancia 
+def squared_primes(array)
+  prime_numbers = array.find_all do |x| 
+    (2..x-1).select{|i| x % i == 0 }.count == 0
+  end
+  prime_numbers.map{|p| p*p}
+end
+
+# Driver code... no edites este texto. En la consola esto deberá imprimir puros trues
+puts shout_backwards("hello, boot") == "TOOB ,OLLEH!!!"
+puts squared_primes([1, 3, 4, 7, 42]) == [1, 9, 49]
